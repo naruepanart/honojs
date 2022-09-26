df6a7b1904ad64403c9840ea5540de1e7300ed82
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2022-09-26 22:38:00 +0700

    *

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,10 @@
 import { Hono } from "hono";
+import { posts } from "./posts";
 
 const app = new Hono();
 
-app.use("*", async (c, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  c.header("X-Response-Time", `${ms}ms`);
-});
-app.notFound((c) => {
-  return c.text("Custom 404 Not Found", 404);
-});
-app.onError((err, c) => {
-  console.error(`${err}`);
-  return c.text("Custom Error Message", 500);
-});
+app.get("/", (c) => c.text("Hello! Hono!"));
 
-// HTTP Methods
-app.get("/", (c) => {
-  return c.text("working");
-});
-app.get("/users", (c) => {
-  return c.json({ get: "get" });
-});
-app.get("/users/:id", (c) => {
-  const id = c.req.param("id");
-  return c.json({ get: id });
-});
-app.post("/users", (c) => {
-  return c.json({ post: "post" });
-});
-app.patch("/users/:id", (c) => {
-  const id = c.req.param("id");
-  return c.json({ patch: id });
-});
-app.delete("/users/:id", (c) => {
-  const id = c.req.param("id");
-  return c.json({ delete: id });
-});
+app.route("/posts", posts);
 
 export default app;
---
+++ src/posts.ts
@@ -0,0 +1,54 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const posts = new Hono();
+
+posts.use("*", async (c, next) => {
+  await next();
+});
+
+posts.use("*", cors());
+
+posts.get("/", (c) => {
+  return c.json({ message: "Hello" });
+});
+
+posts.get("/", (c) => {
+  return c.json({ data: "posts" });
+});
+
+posts.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  if (!id) {
+    return c.json({ error: "Not Found" }, 404);
+  }
+  return c.json({ message: id });
+});
+
+posts.post("/", async (c) => {
+  const body = await c.req.json();
+  return new Response(null, { status: 201 });
+});
+
+posts.patch("/:id", async (c) => {
+  const id = c.req.param("id");
+  if (!id) {
+    return c.json({ error: "Not Found" }, 404);
+  }
+  const body = await c.req.json();
+  if (!body) {
+    return new Response(null, { status: 204 });
+  }
+  return new Response(null, { status: 200 });
+});
+
+posts.delete("/:id", async (c) => {
+  const id = c.req.param("id");
+  if (!id) {
+    return c.json({ error: "Not Found" }, 404);
+  }
+
+  return new Response(null, { status: 200 });
+});
+
+export { posts };
